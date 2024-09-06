581c74c6166485eb4dd7ca9e9acb722ad2bfed62
Mashookh Khan <mashooq.ahmed175@gmail.com>
2024-09-06 22:40:06 +0530

    app post list title

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -3,37 +3,42 @@ import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
 import { AppHeader } from './header/header.component';
 import { FormsModule } from '@angular/forms';
+import { PostsListComponent } from './posts-list/posts-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbar, AppHeader, FormsModule],
+  imports: [RouterOutlet, AppNavbar, AppHeader, FormsModule, PostsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'hotelinventoryapp';
-  isActive : boolean = true
-  username : string = "John Doe"
+  // title = 'hotelinventoryapp';
+  // isActive : boolean = true
+  // username : string = "John Doe"
 
-  userObj : Array<any> = [
-    {id : 1 , name : "John" , email : "john@gmail.com"},
-    {id : 2 , name : "Ritik" , email : "ritik@gmail.com"},
-    {id : 3 , name : "Roshan" , email : "roshan@gmail.com"},
-    {id : 4 , name : "Rahul" , email : "rahul@gmail.com"},
+  // userObj : Array<any> = [
+  //   {id : 1 , name : "John" , email : "john@gmail.com"},
+  //   {id : 2 , name : "Ritik" , email : "ritik@gmail.com"},
+  //   {id : 3 , name : "Roshan" , email : "roshan@gmail.com"},
+  //   {id : 4 , name : "Rahul" , email : "rahul@gmail.com"},
     
-  ]
+  // ]
 
-  addNewUser(){
-    let newUser = {id:5 , name : "User new" , email: "newUser@gmail.com"}
-    this.userObj.push(newUser);
-  }
+  // addNewUser(){
+  //   let newUser = {id:5 , name : "User new" , email: "newUser@gmail.com"}
+  //   this.userObj.push(newUser);
+  // }
 
   // deleteUser(user : object){
   //   let index = this.userObj.indexOf(user);
   //   this.userObj.splice(index,1);
   // }
-  deleteUser(index : number){
-    this.userObj.splice(index,1);
-  }
+  // deleteUser(index : number){
+  //   this.userObj.splice(index,1);
+  // }
+
+  isLoggedIn : boolean = true;
+
+  postTitle : string = "Post 1";
 }
